79ddf6eb06bb044d6d3edc3ffde07d30011f8114
cabidecodes <cabideprogrammingyt@gmail.com>
2024-01-05 21:31:10 -0300

    .

---
+++ Python/projeto00/projeto00.py
@@ -0,0 +1,9 @@
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+def main():
+
+    print("Hello World!")
+
+    return 0
+if __name__ == "__main__":
+    main()
---
+++ Python/projeto01/projeto01.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+def main():
+
+    number = int(input("Digite um número: \n"))
+
+    print(f"O número informado foi: {number}")
+
+    return 0
+if __name__ == "__main__":
+    main()
