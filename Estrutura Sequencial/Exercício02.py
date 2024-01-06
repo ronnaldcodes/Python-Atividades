cc9b44ec73f96e7937f3a53113310e6d1ed0efb4
cabidecodes <cabideprogrammingyt@gmail.com>
2024-01-06 18:51:59 -0300

    02

---
+++ Estrutura Sequencial/Exercício02.py
@@ -0,0 +1,13 @@
+# Faça um Programa que peça dois números e imprima a soma.
+def main():
+
+    a = int(input("Digite um número:\n"))
+    b = int(input("Agora digite outro número:\n"))
+
+    print(f'A soma de {a} + {b} = {a + b}')
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
